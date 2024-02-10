13b0c28c5d08fefb629c8a38c7ce696eb72308a3
jackrose27 <157540395+jackrose27@users.noreply.github.com>
2024-02-09 19:50:13 -0600

    Update main.py
    This is the updated version of my assignment.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   char_value = word[index]
+   numbers = ("0","1","2","3","4","5","6","7","8","9")
+   whitespace = " "
+   alphabet = char_value.isalpha()
+
+   if char_value == whitespace:
+       return "white space"
+   elif char_value in numbers:
+       return "digit"
+   elif alphabet == True:
+       return "letter"
+   else:
+       return "unknown"
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
